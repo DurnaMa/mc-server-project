572e324dead54f6269b816cea7c7fa848ed076d3
Mahir Durna <durna.m@outlook.de>
2026-07-28 21:41:28 +0200

    Make server.properties configurable via envsubst template

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,11 @@ FROM eclipse-temurin:25-jre
 
 WORKDIR /minecraft
 
-RUN apt-get update && apt-get install -y wget && wget https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar -O server.jar
+RUN apt-get update && apt-get install -y wget gettext-base && wget https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar -O server.jar && rm -rf /var/lib/apt/lists/*
 
 COPY . .
 
 RUN chmod +x entrypoint.sh
-
-ENV MINECRAFT_PORT=25565
 
 EXPOSE ${MINECRAFT_PORT}
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# Port aus ENV in die properties schreiben:
-sed -i "s/^server-port=.*/server-port=${MINECRAFT_PORT}/" server.properties
-# dann erst den Server starten:
-exec java -Xmx4G -Xms4G -jar server.jar nogui
+
+# 1.Defaults setzen
+: "${MINECRAFT_PORT:=25565}"
+: "${MAX_PLAYERS:=20}"
+: "${MOTD:=A Minecraft Server}"
+: "${DIFFICULTY_LEVEL:=easy}"
+: "${MAX_MEMORY:=1536M}"
+
+export MINECRAFT_PORT MAX_PLAYERS MOTD DIFFICULTY_LEVEL
+
+envsubst < server.properties.template > server.properties
+
+exec java -Xmx${MAX_MEMORY} -Xms${MAX_MEMORY} -jar server.jar nogui
